f10983b7b29fd3200f256ed347701e82c89d9b41
Knut Nesheim <knutin@gmail.com>
2012-09-19 18:10:36 +0200

    Implemented bisect:delete/2. Added insert speed test.

--- src/bisect.erl
+++ src/bisect.erl
@@ -14,11 +14,12 @@
 -module(bisect).
 -author('Knut Nesheim <knutin@gmail.com>').
 
--export([new/2, insert/3, find/2, update/3]).
+-export([new/2, insert/3, find/2, update/3, delete/2, compact/1]).
 -export([serialize/1, deserialize/1, from_orddict/2, find_many/2]).
 -export([expected_size/2, expected_size_mb/2, num_keys/1]).
 
 -compile({no_auto_import, [size/1]}).
+-compile(native).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -28,7 +29,6 @@
 %%
 %% TYPES
 %%
-
 
 -type key_size()   :: pos_integer().
 -type value_size() :: pos_integer().
@@ -55,49 +55,11 @@
 -spec new(key_size(), value_size()) -> bindict().
 %% @doc: Returns a new empty dictionary where where the keys and
 %% values will always be of the given size.
-new(KeySize, ValueSize) ->
+new(KeySize, ValueSize) when is_integer(KeySize) andalso is_integer(ValueSize) ->
     #bindict{key_size = KeySize,
              value_size = ValueSize,
              block_size = KeySize + ValueSize,
              b = <<>>}.
-
-
-%% @doc: Uses binary search to find the index of the given key. If the
-%% key does not exist, the index where it should be inserted is
-%% returned.
--spec index(bindict(), key()) -> index().
-index(<<>>, _) ->
-    0;
-index(B, K) ->
-    N = byte_size(B#bindict.b) div B#bindict.block_size,
-    index(B, 0, N, K).
-
-index(_B, Low, High, _K) when High =:= Low ->
-    Low;
-
-index(_B, Low, High, _K) when High < Low ->
-    -1;
-
-index(B, Low, High, K) ->
-    Mid = (Low + High) div 2,
-    MidOffset = index2offset(B, Mid),
-
-    KeySize = B#bindict.key_size,
-    case byte_size(B#bindict.b) > MidOffset of
-        true ->
-            <<_:MidOffset/binary, MidKey:KeySize/binary, _/binary>> = B#bindict.b,
-
-            if
-                MidKey > K ->
-                    index(B, Low, Mid, K);
-                MidKey < K ->
-                    index(B, Mid + 1, High, K);
-                MidKey =:= K ->
-                    Mid
-            end;
-        false ->
-            Mid
-    end.
 
 
 -spec insert(bindict(), key(), value()) -> bindict().
@@ -156,7 +118,25 @@ find(B, K) ->
 find_many(B, Keys) ->
     lists:map(fun (K) -> find(B, K) end, Keys).
 
+-spec delete(bindict(), key()) -> bindict().
+delete(B, K) ->
+    LeftOffset = index2offset(B, index(B, K)),
+    KeySize = B#bindict.key_size,
+    ValueSize = B#bindict.value_size,
 
+    case B#bindict.b of
+        <<Left:LeftOffset/binary, K:KeySize/binary, _:ValueSize/binary, Right/binary>> ->
+            B#bindict{b = <<Left/binary, Right/binary>>};
+        _ ->
+            erlang:error(badarg)
+    end.
+
+
+%% @doc: Compacts the internal binary used for storage, by creating a
+%% new copy where all the data is aligned in memory. Writes will cause
+%% fragmentation.
+compact(B) ->
+    B#bindict{b = binary:copy(B#bindict.b)}.
 
 %% @doc: Returns how many bytes would be used by the structure if it
 %% was storing NumKeys.
@@ -210,6 +190,43 @@ from_orddict(#bindict{b = <<>>} = B, Orddict) ->
 index2offset(_, 0) -> 0;
 index2offset(B, I) -> I * B#bindict.block_size.
 
+%% @doc: Uses binary search to find the index of the given key. If the
+%% key does not exist, the index where it should be inserted is
+%% returned.
+-spec index(bindict(), key()) -> index().
+index(<<>>, _) ->
+    0;
+index(B, K) ->
+    N = byte_size(B#bindict.b) div B#bindict.block_size,
+    index(B, 0, N, K).
+
+index(_B, Low, High, _K) when High =:= Low ->
+    Low;
+
+index(_B, Low, High, _K) when High < Low ->
+    -1;
+
+index(B, Low, High, K) ->
+    Mid = (Low + High) div 2,
+    MidOffset = index2offset(B, Mid),
+
+    KeySize = B#bindict.key_size,
+    case byte_size(B#bindict.b) > MidOffset of
+        true ->
+            <<_:MidOffset/binary, MidKey:KeySize/binary, _/binary>> = B#bindict.b,
+
+            if
+                MidKey > K ->
+                    index(B, Low, Mid, K);
+                MidKey < K ->
+                    index(B, Mid + 1, High, K);
+                MidKey =:= K ->
+                    Mid
+            end;
+        false ->
+            Mid
+    end.
+
 
 %%
 %% TEST
@@ -260,6 +277,13 @@ update_non_existing_test() ->
     ?assertError(badarg, update(B, <<3:64/integer>>, <<4>>)).
 
 
+delete_test() ->
+    B = insert_many(new(8, 1), [{2, 2}, {3, 3}, {1, 1}]),
+    ?assertEqual(<<2:8/integer>>, find(B, ?i2k(2))),
+
+    NewB = delete(B, ?i2k(2)),
+    ?assertEqual(not_found, find(NewB, ?i2k(2))).
+
 size_test() ->
     Start = 100000000000000,
     N = 1000,
@@ -284,7 +308,7 @@ speed_test_() ->
     {timeout, 600,
      fun() ->
              Start = 100000000000000,
-             N = 10000,
+             N = 25000,
              Keys = lists:seq(Start, Start+N),
              KeyValuePairs = lists:map(fun (I) -> {<<I:64/integer>>, <<255:8/integer>>} end,
                                        Keys),
@@ -296,11 +320,34 @@ speed_test_() ->
      end}.
 
 
+insert_speed_test_() ->
+    {timeout, 600,
+     fun() ->
+             Start = 100000000000000,
+             N = 50000,
+             Keys = lists:seq(Start, Start+N),
+             KeyValuePairs = lists:map(fun (I) -> {<<I:64/integer>>, <<255:8/integer>>} end,
+                                       Keys),
+             ReadKeys = [lists:nth(random:uniform(N), Keys) || _ <- lists:seq(1, 1000)],
+
+             StartTime = now(),
+             B = lists:foldl(fun ({K, V}, B) ->
+                                 insert(B, K, V)
+                         end, new(8, 1), KeyValuePairs),
+             ElapsedUs = timer:now_diff(now(), StartTime),
+             error_logger:info_msg("insert in ~p ms, ~p us per key~n",
+                                   [ElapsedUs / 1000,
+                                    ElapsedUs / N
+                                   ]),
+             time_reads(B, N, ReadKeys)
+     end}.
+
+
 time_reads(B, Size, ReadKeys) ->
     Parent = self(),
     spawn(
       fun() ->
-              Runs = 20,
+              Runs = 100,
               Timings =
                   lists:map(
                     fun (_) ->
@@ -309,7 +356,7 @@ time_reads(B, Size, ReadKeys) ->
                             timer:now_diff(now(), StartTime)
                     end, lists:seq(1, Runs)),
 
-              Rps = 1000000 / ((lists:sum(Timings) / length(Timings)) / 1000),
+              Rps = 1000000 / ((lists:sum(Timings) / length(Timings)) / length(ReadKeys)),
               error_logger:info_msg("Average over ~p runs, ~p keys in dict~n"
                                     "Average fetch ~p keys: ~p us, max: ~p us~n"
                                     "Average fetch 1 key: ~p us~n"

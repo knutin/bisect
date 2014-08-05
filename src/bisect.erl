3c5203b90fb571d2a8e4aa12caf11145905a4aa5
Knut Nesheim <knutin@gmail.com>
2014-08-05 16:00:07 +0200

    Fast key intersection in many bisects.

--- src/bisect.erl
+++ src/bisect.erl
@@ -16,7 +16,8 @@
 
 -export([new/2, new/3, insert/3, bulk_insert/2, append/3, find/2, foldl/3]).
 -export([next/2, next_nth/3, first/1, last/1, delete/2, compact/1, cas/4, update/4]).
--export([serialize/1, deserialize/1, from_orddict/2, to_orddict/1, find_many/2, merge/2]).
+-export([serialize/1, deserialize/1, from_orddict/2, to_orddict/1, find_many/2]).
+-export([merge/2, intersection/1, intersection/2]).
 -export([expected_size/2, expected_size_mb/2, num_keys/1, size/1]).
 
 -compile({no_auto_import, [size/1]}).
@@ -346,20 +347,81 @@ do_merge(Small, Big, Acc, KeySize, ValueSize) ->
             lists:reverse([Big | Acc])
     end.
 
+%% @doc: Intersect two or more bindicts by key. The resulting bindict
+%% contains keys found in all input bindicts.
+intersection(Bs) when length(Bs) >= 2 ->
+    intersection(Bs, svs);
+intersection(_TooFewSets) ->
+    erlang:error(badarg).
+
+%% @doc: SvS set intersection algorithm, as described in
+%% http://www.cs.toronto.edu/~tl/papers/fiats.pdf
+intersection(Bs, svs) ->
+    [CandidateSet | Sets] = lists:sort(fun (A, B) -> size(A) =< size(B) end, Bs),
+    from_orddict(new(CandidateSet#bindict.key_size,
+                     CandidateSet#bindict.value_size),
+                 do_svs(Sets, CandidateSet)).
+
+do_svs([], Candidates) ->
+    Candidates;
+do_svs([Set | Sets], #bindict{} = Candidates) ->
+    %% Optimization: we let the candidate set remain a bindict for the
+    %% first iteration to avoid creating a large orddict just to throw
+    %% most of it away. For the remainding sets, we keep the candidate
+    %% set as a list
+    {_, NewCandidatesList} =
+        foldl(Candidates,
+              fun (K, V, {L, Acc}) ->
+                      Size = byte_size(Set#bindict.b) div Set#bindict.block_size,
+                      Rank = index(Set, L, Size, K),
+                      %% TODO: Skip candidates until OtherK?
+                      case at(Set, Rank) of
+                          {K, _}       -> {Rank, [{K, V} | Acc]};
+                          {_OtherK, _} -> {Rank, Acc};
+                          not_found    -> {Rank, Acc}
+                      end
+              end, {0, []}),
+    do_svs(Sets, lists:reverse(NewCandidatesList));
+
+do_svs([Set | Sets], Candidates) when is_list(Candidates) ->
+    {_, NewCandidates} =
+        lists:foldl(fun ({K, V}, {L, Acc}) ->
+                            Size = byte_size(Set#bindict.b) div Set#bindict.block_size,
+                            Rank = index(Set, L, Size, K),
+                            case at(Set, Rank) of
+                                {K, _}       -> {Rank, [{K, V} | Acc]};
+                                {_OtherK, _} -> {Rank, Acc};
+                                not_found    -> {Rank, Acc}
+                            end
+                    end, {0, []}, Candidates),
+    do_svs(Sets, lists:reverse(NewCandidates)).
+
+at(B, I) ->
+    Offset = index2offset(B, I),
+    KeySize = B#bindict.key_size,
+    ValueSize = B#bindict.value_size,
+    case B#bindict.b of
+        <<_:Offset/binary, Key:KeySize/binary, Value:ValueSize/binary, _/binary>> ->
+            {Key, Value};
+        _ ->
+            not_found
+    end.
+
+
 %% @doc: Populates the dictionary with data from the orddict, taking
 %% advantage of the fact that it is already ordered. The given bindict
 %% must be empty, but contain size parameters.
 from_orddict(#bindict{b = <<>>} = B, Orddict) ->
     KeySize = B#bindict.key_size,
     ValueSize = B#bindict.value_size,
-    NewB = lists:foldl(fun ({K, V}, Bin)
-                             when byte_size(K) =:= B#bindict.key_size andalso
-                                  byte_size(V) =:= B#bindict.value_size ->
-                               <<Bin/binary, K:KeySize/binary, V:ValueSize/binary>>;
-                           (_, _) ->
-                               erlang:error(badarg)
-                       end, B#bindict.b, orddict:to_list(Orddict)),
-    B#bindict{b = NewB}.
+    L = orddict:fold(fun (K, V, Acc)
+                           when byte_size(K) =:= B#bindict.key_size andalso
+                                byte_size(V) =:= B#bindict.value_size ->
+                             [<<K:KeySize/binary, V:ValueSize/binary>> | Acc];
+                         (_, _, _) ->
+                             erlang:error(badarg)
+                     end, [], Orddict),
+    B#bindict{b = iolist_to_binary(lists:reverse(L))}.
 
 to_orddict(#bindict{} = B) ->
     lists:reverse(
@@ -520,6 +582,10 @@ last_test() ->
     KV2 = {K2, V2} = {<<3:64>>, <<3:8>>},
     B1 = new(8, 1),
     ?assertEqual(not_found, last(B1)),
+    ?assertEqual(0, num_keys(B1)),
+    ?assertEqual(not_found, at(B1, 0)),
+    ?assertEqual(not_found, at(B1, -1)),
+    ?assertEqual(not_found, at(B1, 1)),
     B2 = insert(B1, K1, V1),
     ?assertEqual(KV1, last(B2)),
     B3 = insert(B2, K2, V2),
@@ -562,11 +628,90 @@ from_orddict_test() ->
     ?assertEqual(<<255>>, find(from_orddict(new(8, 1), Orddict), <<1:64/integer>>)).
 
 
+intersection_test() ->
+    Sets = [insert_many(new(8, 1), [{1, 1}, {2, 2}, {3, 3}]),
+            insert_many(new(8, 1), [{1, 1}, {2, 3}, {4, 4}]),
+            insert_many(new(8, 1), [{1, 1}, {2, 3}, {5, 5}]),
+            insert_many(new(8, 1), [{1, 1}, {2, 3}, {6, 6}])],
+
+    Intersection = intersection(Sets),
+    ?assertEqual(to_orddict(insert_many(new(8, 1), [{1, 1}, {2, 2}])),
+                 to_orddict(Intersection)).
+
+
+intersection_perf_test_() ->
+    {timeout, 600, ?_test(intersection_perf())}.
+
+intersection_perf() ->
+    TestCases = [{[1000, 1000], 10},
+                 {[100000, 100000, 100000], 1000},
+                 {[10000, 100000, 1000000], 1000},
+                 {[1000000, 1000000, 1000000], 10000}
+                ],
+
+    lists:foreach(
+      fun ({SetSizes, IntersectionSize}) ->
+              UnionSize = lists:sum([SetSize - IntersectionSize
+                                     || SetSize <- SetSizes]) + IntersectionSize,
+              KVs = lists:map(fun (K) -> {<<K:36/binary>>, <<97:32/integer>>} end,
+                              generate_unique(UnionSize)),
+              ?assertEqual(UnionSize, sets:size(sets:from_list(KVs))),
+
+              {IntersectionKeys, Rest} = lists:split(IntersectionSize, KVs),
+              {SetKeys, []} = lists:mapfoldl(fun (Size, AccRest) ->
+                                                     lists:split(Size - IntersectionSize,
+                                                                 AccRest)
+                                             end, Rest, SetSizes),
+              ?assertEqual(IntersectionSize, length(IntersectionKeys)),
+
+              SetIntersection = sets:intersection(
+                                  [sets:from_list(Ks ++ IntersectionKeys)
+                                   || Ks <- SetKeys]),
+              ?assertEqual(IntersectionSize, sets:size(SetIntersection)),
+
+              Bisects = lists:map(fun (Ks) ->
+                                          AllKeys = orddict:from_list(
+                                                      Ks ++ IntersectionKeys),
+                                          from_orddict(new(36, 4), AllKeys)
+                                  end, SetKeys),
+              {IntersectUs, BisectIntersection} = timer:tc(
+                                                    fun () -> intersection(Bisects) end),
+              IntersectingKeys = to_orddict(BisectIntersection),
+              ?assertEqual(length(lists:sort(sets:to_list(SetIntersection))),
+                           length(lists:sort(IntersectingKeys))),
+              ?assertEqual(lists:sort(sets:to_list(SetIntersection)),
+                           lists:sort(IntersectingKeys)),
+              error_logger:info_msg("Set sizes: ~p, Intersection size: ~p~n"
+                                    "Intersection runtime: ~.2f ms~n",
+                                    [SetSizes, IntersectionSize,
+                                     IntersectUs / 1000]),
+
+              ok
+      end, TestCases).
+
+
+generate_unique(N) ->
+    RandomGenerator = fun () -> crypto:rand_bytes(36) end,
+    generate_unique(RandomGenerator, [], N).
+
+generate_unique(RandomGenerator, Acc, N) ->
+    case length(Acc) =:= N of
+        true ->
+            Acc;
+        false ->
+            Gen = fun (_, 0) -> [];
+                      (F, M) -> [RandomGenerator() | F(F, M-1)]
+                  end,
+            Uniques = lists:usort(Gen(Gen, N - length(Acc))),
+            generate_unique(RandomGenerator, Acc ++ Uniques, N)
+    end.
+
+
 speed_test_() ->
     {timeout, 600,
      fun() ->
              Start = 100000000000000,
-             N = 10000,
+             N = 100000,
              Keys = lists:seq(Start, Start+N),
              KeyValuePairs = lists:map(fun (I) -> {<<I:64/integer>>, <<255:8/integer>>} end,
                                        Keys),
